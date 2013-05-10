bf4b8ba43ece501246180797690167eb57f426d9
BogDan Vatra <bogdan@kde.org>
2013-05-10 20:06:15 +0300

    update run script

--- ministrorepogen/run.sh
+++ ministrorepogen/run.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-ARCHITECTURE=armeabi-v7a # armeabi x86 mips
-MINISTRO_REPO_VERSION=5.103
-QT_PATH=$PWD/Qt/$ARCHITECTURE/
-READELF=/root/necessitas/android-ndk/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86_64/bin/arm-linux-androideabi-readelf
+MINISTRO_REPO_VERSION=5.104
+ANDROID_NDK_PATH=/root/necessitas/android-ndk
+READELF=$ANDROID_NDK_PATH/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86_64/bin/arm-linux-androideabi-readelf
 RULES=rules.xml
-OUT_PATH=$PWD/unstable
-OBJECTS_REPO=$MINISTRO_REPO_VERSION-$ARCHITECTURE
-OBJECTS_PATH=$PWD/objects
-QT_VERSION=$((0x050100))
+QT_BINS=/opt/Qt5.1.0/5.1.0-beta1
 
-./ministrorepogen $READELF $QT_PATH $MINISTRO_REPO_VERSION $ARCHITECTURE $RULES $OUT_PATH $OBJECTS_REPO $QT_VERSION
-mkdir -p $OBJECTS_PATH
-rm -fr $OBJECTS_PATH/$OBJECTS_REPO
-cp -a $QT_PATH $OBJECTS_PATH/$OBJECTS_REPO
-#./ministrorepogen /root/necessitas/android-ndk/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86_64/bin/arm-linux-androideabi-readelf $PWD/Qt 5.1 armeabi-v7a rules.xml $PWD/unstable armeabi-v7a-5.1 327936
+create_repo()
+{
+    ARCHITECTURE=$1 #armeabi-v7a # armeabi x86 mips
+    QT_PATH=$PWD/Qt/$ARCHITECTURE/
+    OUT_PATH=$PWD/unstable
+    OBJECTS_REPO=$MINISTRO_REPO_VERSION-$ARCHITECTURE
+    OBJECTS_PATH=$PWD/objects
+    QT_VERSION=$((0x050100))
+
+    ./ministrorepogen $READELF $QT_PATH $MINISTRO_REPO_VERSION $ARCHITECTURE $RULES $OUT_PATH $OBJECTS_REPO $QT_VERSION
+
+    mkdir -p $OBJECTS_PATH
+    rm -fr $OBJECTS_PATH/$OBJECTS_REPO
+    cp -a $QT_PATH $OBJECTS_PATH/$OBJECTS_REPO
+}
+
+copy_qt()
+{
+    mkdir -p Qt/$2/lib
+    cp -L $1/lib/*.so Qt/$2/lib
+    cp $ANDROID_NDK_PATH/sources/cxx-stl/gnu-libstdc++/4.7/libs/$2/libgnustl_shared.so Qt/$2/lib/
+    cp -a $1/imports Qt/$2
+    cp -a $1/jar Qt/$2
+    cp -a $1/plugins Qt/$2
+    cp -a $1/qml Qt/$2
+}
+
+rm -fr Qt
+rm -fr objects
+rm -fr unstable
+
+copy_qt $QT_BINS/android_armv7 armeabi-v7a
+copy_qt $QT_BINS/android_x86 x86
+
+create_repo armeabi-v7a
+create_repo x86
